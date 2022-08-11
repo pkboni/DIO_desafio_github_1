1bf8b071ec45ac9516da778ab364d13db9cb75d8
pkboni <90481573+pkboni@users.noreply.github.com>
2022-08-11 13:54:21 -0300

    Desafio_TypeScript - desafio1
    desafio1 recodificado.

--- Desafio_TypeScript.js
+++ Desafio_TypeScript.js
@@ -1,29 +1,24 @@
+"use strict";
 // Como podemos rodar isso em um arquivo .ts sem causar erros? 
 /*
 let employee = {};
 employee.code = 10;
 employee.name = "John";
 */
-
-let worker1: {code1: number, name1: string}={
+let worker1 = {
     code1: 10,
     name1: "John"
 };
 console.log(worker1.code1, worker1.name1);
-
 let worker2 = {
     code2: 11,
     name2: "Philip"
 };
 console.log(worker2.code2, worker2.name2);
-
-interface Worker {
-    code3: number,
-    name3: string
-};
-
-function getWorker(work: Worker){
+;
+function getWorker(work) {
     return `${work.code3} ${work.name3}`;
 }
-let walterTiir = {code3: 12, name3: 'Walter Tiir'};
+let walterTiir = { code3: 12, name3: 'Walter Tiir' };
 console.log(getWorker(walterTiir));
+
